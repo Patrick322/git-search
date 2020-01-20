7a6a23f89b38f5908097ad69950b6ecba1d14172
Patrick595 <patrickbuong@gmail.com>
2020-01-20 09:43:13 +0300

    modified the application

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -7,7 +7,13 @@ import { HttpClient } from '@angular/http'
 export class ProfileService {
 
   private username:string;
+  private clientid = 'e7649e9d21074f39c4c5';
+  private clientsecret = '7ba2d8c0511a60ef64ea01ae45b1c2f888e5e9ae';
   constructor(private http:Http) {
-    
+   console.log("service is now ready!"); 
+   this.username = 'Patrick322'
+   }
+   getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid +) 
    }
 }
